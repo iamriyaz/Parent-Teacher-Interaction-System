907204de2cc9d1fe0b2f4483fb44a834034b84fa
iamriyaz <riyasthengil41@gmail.com>
2019-09-24 17:02:52 +0530

    db file updated

--- db/ptis.sql
+++ db/ptis.sql
@@ -176,10 +176,7 @@ CREATE TABLE IF NOT EXISTS `groupmsg` (
 -- Dumping data for table `groupmsg`
 --
 
-INSERT INTO `groupmsg` (`id`, `sub`, `msg`, `from`, `to`, `division`) VALUES
-(1, 'ytrhy', 'ytrhy', 'ytrhy', 'ytrhy', ''),
-(2, 'hi', 'jhgjhg', 'swapna123', '8', 'C'),
-(3, 'hi', 'hgfjh', 'swapna123', '8', 'C');
+
 
 -- --------------------------------------------------------
 
@@ -207,15 +204,7 @@ CREATE TABLE IF NOT EXISTS `parent` (
 -- Dumping data for table `parent`
 --
 
-INSERT INTO `parent` (`Ration_card_no`, `first_name`, `last_name`, `address`, `phone_no`, `e_mail`, `relation`, `gender`, `occupation`, `username`, `password`, `verified`) VALUES
-('', 'ajay', 'anand', 'tyggh', '123456678', 'a@g.c', 'Father', 'Male', 'director', 'ajay1', '123456', 1),
-('', 'dfsd', 'dfgd', 'dfgfh\r\nftyj\r\n', '3456787654', 'vbcvbvf', 'Father', 'Other', 'fxfgb', 'asd', 'asdfgh', 1),
-('', 'biju', 'vr', 'ambadi housecherakkapparamb postangadippuramperinthalmannamalappuramkerala', '9867753244', 'biju123@gmail.c', 'Father', 'Male', 'police officer', 'biju123', 'biju123', 1),
-('', 'raman', 'v', 'vailongara', '8790876543', 'raman12@gmail.c', 'Father', 'Male', 'bjhcysdfcuy', 'raman', '1234567', 1),
-('', 'Ravindran', 'V', 'Vailongara\r\ncherakkapparamb\r\nangadippuram\r\nperinthalamanna', '9539150320', 'ravindran@g.c', 'Father', 'Male', 'Dance Master', 'ravi123', 'kkkkk', 1),
-('', 'ravindran', 'v', 'vailonrgar \r\ncherakkapparamb\r\nangadippuram', '9539150320', 'ravindran@gmail', 'Father', 'Male', 'dance master', 'ravindran123', 'zxcvb', 1),
-('', 'sukumaran', 't', 'thekknkittil houe', '8075344314', 'sukumaran123@gm', 'Father', 'Male', 'driver', 'suku', 'suku', 1),
-('', 'vijay', 'v', 'crgvcgv ', '4567898765', 'vgfdg  kjj', 'Gardian', 'Male', 'biu', 'vij12', 'asdfghjkl', 1);
+
 
 -- --------------------------------------------------------
 
@@ -237,8 +226,7 @@ CREATE TABLE IF NOT EXISTS `scoresheet` (
 -- Dumping data for table `scoresheet`
 --
 
-INSERT INTO `scoresheet` (`ID`, `RegNO`, `Subject`, `Mark`, `Grade`, `Teacher`) VALUES
-(1, '36', 14, '25.00', 'C', 'swapna123');
+
 
 -- --------------------------------------------------------
 
@@ -268,15 +256,7 @@ CREATE TABLE IF NOT EXISTS `student` (
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`Ration_card_no`, `reg_no`, `roll_no`, `name`, `addres`, `admn_date`, `gender`, `dob`, `parent_username`, `standard`, `division`, `blood_group`, `religion`, `caste`) VALUES
-('', '34', 0, 'ghmncgh', 'hjm,hj', '0000-00-00', 'Male', '0000-00-00', '', 10, 'A', 'B+', 'tyjyt', 'tyjtyj'),
-('', '36', 10, 'riyas', 'melathodu house\r\nkaringallathani\r\nkerala', '2016-09-06', 'Male', '2015-03-02', 'suku', 8, 'C', 'O-', 'islam', 'h'),
-('', '456', 7, 'gireesh', 'aalakkat house\r\ncherrakkaparambu post\r\nangadippuram via\r\nmalappuram dt', '2015-02-24', 'Male', '1996-10-24', 'suku', 9, 'A', 'O+', 'hindu', 'nair'),
-('', 'bh1234', 5, 'aju', 'hgf', '2014-02-23', 'Male', '2015-02-23', 'ajay1', 6, 'A', 'A+', 'h', 'n'),
-('', 'nza12345', 12, 'jaggu', 'gghh', '2018-02-23', 'Male', '2017-02-23', 'vij12', 7, 'A', 'A+', 'h', 'n'),
-('', 'nzapbca009', 12, 'ramesh', 'thekkethil house\r\ncherakkapparambu po\r\nangadippuram via\r\nmalappuram dt\r\n679321 pin', '2016-01-19', 'Male', '1997-12-25', 'ravi123', 10, 'A', 'O+', 'hindu', 'nair'),
-('', 'nzapbca016', 0, 'vishnu', 'vailoh\r\nang\r\nperin', '2016-01-13', 'Male', '1996-09-06', '', 6, 'C', 'A-', 'h', 'a'),
-('', 'reg666', 0, 'vvvvv', 'thhjhf\r\nxghgjk\r\nbfvvbhfgy\r\njhijmk', '2009-06-07', 'Male', '1998-07-08', '', 3, 'C', 'O+', 'hgff', 'uygf');
+
 
 -- --------------------------------------------------------
 
@@ -331,10 +311,7 @@ CREATE TABLE IF NOT EXISTS `teacher` (
 -- Dumping data for table `teacher`
 --
 
-INSERT INTO `teacher` (`faculty_no`, `first_name`, `last_name`, `address`, `phone_no`, `e_mail`, `experience`, `qualification`, `gender`, `is_class_teacher`, `standard`, `division`, `username`, `password`, `subject`) VALUES
-(23, 'adc sx', 'cv zdf', 'dgbsfhng\r\nfhnx\r\nfvgh mn', '4567865456', 'ghncfghm', 3, 'fgn cxfghmncgh\r\nhmgvbmvjh\r\n', 'Female', 0, NULL, NULL, 'zxdfgbzxdf', 'qwert', 'cv zdf'),
-(33, 'summa', 'ya', 'dffgfghfg\r\nghjfhgj\r\ngyjgh\r\n', '365464565476', 'grdgn@g.c', 3, 'phd', 'Female', 0, NULL, NULL, 'suma123', 'qwerty', 'ya'),
-(223, 'swapnaa', 'mohan', 'vattamkulangarea hmalappuramkerala', '9856755432', 'swapnamohan123@gmail.com', 5, 'pgphd', 'Female', 0, '12', 'B', 'swapna123', 'asdfg', 'mohan');
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
